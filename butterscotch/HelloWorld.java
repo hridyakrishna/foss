c7b3e0d37ca086cee85e97818d660f5232efbd1d
hridya <hridyakrishnar127@gmail.com>
2018-11-07 17:18:51 +0530

    my first java program

---
+++ butterscotch/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+This is my first java program.
+I am a learner and got the basics from my senior Abhilash bhayya and also from the java documentation of oracle.com
+I started with the HelloWorld application.
+*/
+public class HelloWorld {
+  public static void main(String[] args){
+      System.out.println("Hello World: Butterscotch");
+  }
+}
